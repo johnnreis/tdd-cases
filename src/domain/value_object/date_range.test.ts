e95b827885becd698dcbac6731f4cd3e273248f8
John Reis <jhonatareisy7@gmail.com>
2025-03-26 01:17:36 -0300

    refactor: refactoring constructor

--- src/domain/value_object/date_range.test.ts
+++ src/domain/value_object/date_range.test.ts
@@ -3,24 +3,46 @@ import { DateRange } from "./date_range";
 describe("DateRange Value Object", () => {
   it("should throw an error if the end date is before the start date", () => {
     expect(() => {
-      new DateRange(new Date("2024-12-25"), new Date("2024-12-20"));
-    }).toThrow("a data de termino deve ser posterior a data de inicio");
+      new DateRange(new Date("2025-12-25"), new Date("2025-12-20"));
+    }).toThrow("a data de termino deve ser posterior a data de inicio.");
   });
   it("should create a DateRange instance with the start date and end date, \
     and check the return", () => {
-    const startDate = new Date("2024-12-20");
-    const endDate = new Date("2024-12-25");
+    const startDate = new Date("2025-12-20");
+    const endDate = new Date("2025-12-25");
     const dateRange = new DateRange(startDate, endDate);
     expect(dateRange.getStartDate()).toEqual(startDate);
     expect(dateRange.getEndDate()).toEqual(endDate);
   });
 
   it("should calculate the total nights correctly", () => {
-    const startDate = new Date("2024-12-20");
-    const endDate = new Date("2024-12-25");
+    const startDate = new Date("2025-12-20");
+    const endDate = new Date("2025-12-25");
     const dateRange = new DateRange(startDate, endDate);
     const totalNights = dateRange.getTotalNights();
 
     expect(totalNights).toBe(5);
+  });
+
+  it("should check if two date ranges overlap", () => {
+    const dateRange1 = new DateRange(
+      new Date("2025-12-22"),
+      new Date("2025-12-27")
+    );
+
+    const dateRange2 = new DateRange(
+      new Date("2025-12-22"),
+      new Date("2025-12-27")
+    );
+
+    const overlaps = dateRange1.overlaps(dateRange2);
+    expect(overlaps).toBe(true);
+  });
+
+  it("should throw error if startDate & endDate to Equals", () => {
+    const date = new Date("2025-12-20");
+    expect(() => {
+      new DateRange(date, date);
+    }).toThrow("a data de incio e termino nao podem ser iguais");
   });
 });
--- src/domain/value_object/date_range.ts
+++ src/domain/value_object/date_range.ts
@@ -3,12 +3,9 @@ export class DateRange {
   private readonly endDate: Date;
 
   constructor(startDate: Date, endDate: Date) {
+    this.validateDates(startDate, endDate);
     this.startDate = startDate;
     this.endDate = endDate;
-
-    if (endDate <= startDate) {
-      throw new Error("a data de termino deve ser posterior a data de inicio");
-    }
   }
 
   getStartDate(): Date {
@@ -22,5 +19,20 @@ export class DateRange {
   getTotalNights(): number {
     const diffTime = this.endDate.getTime() - this.startDate.getTime();
     return Math.ceil(diffTime / (1000 * 3600 * 24));
+  }
+
+  overlaps(other: DateRange): boolean {
+    return (
+      this.startDate < other.endDate && other.getStartDate() < this.endDate
+    );
+  }
+
+  private validateDates(startDate: Date, endDate: Date): void {
+    if (startDate === endDate) {
+      throw new Error("a data de incio e termino nao podem ser iguais");
+    }
+    if (endDate < startDate) {
+      throw new Error("a data de termino deve ser posterior a data de inicio.");
+    }
   }
 }
